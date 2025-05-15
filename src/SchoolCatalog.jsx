93307b53cd944a1a4698c7e851d93fca9281345c
eliBrank <ezb.business@pm.me>
2025-05-14 22:14:04 -0500

    get sorting working for each column

--- src/SchoolCatalog.jsx
+++ src/SchoolCatalog.jsx
@@ -1,30 +1,52 @@
 import { useState, useEffect, useMemo } from 'react';
 
 export default function SchoolCatalog() {
-  const [courseData, setCourseData] = useState([]);
+  const [coursesData, setCoursesData] = useState([]);
   const [filterText, setFilterText] = useState('');
-  const [sortColumn, setSortColumn] = useState('Trimester');
+  const [sortField, setSortField] = useState('trimester');
   const [sortAsc, setSortAsc] = useState(true);
 
   useEffect(() => {
     fetch('/api/courses.json')
       .then((response) => response.json())
       .then((data) => {
-        setCourseData(data);
+        setCoursesData(data);
       });
   }, []);
 
+  const handleSort = (field) => {
+    setSortField(field);
+    // sortAsc set to false if same sort re-triggered, otherwise true
+    setSortAsc((sortField === field && sortAsc) ? false : true);
+  }
+
 
   const coursesProcessed = useMemo(() => {
-    const coursesFiltered = courses.filter((course) => {
+    const coursesFiltered = coursesData.filter((course) => {
       return course.courseNumber.startsWith(filterText) ||
         course.courseName.startsWith(filterText);
     });
 
-    const coursesSorted = coursesFiltered.sort(a, b) {
+    // spread operator used on coursesFiltered to create copy
+    // so that sort method does not alter it directly
+    const coursesSorted = [...coursesFiltered].sort((a, b) => {
+      const sortModifier = sortAsc ? 1 : -1;
+      const stringSortFields = [
+        'courseNumber',
+        'courseName',
+      ];
 
-    }
-  });
+      // process for sorting strings, numbers is different
+      // so check which the column is using
+      if (stringSortFields.includes(sortField)) {
+        return a[sortField].localeCompare(b[sortField]) * sortModifier;
+      }
+      return (a[sortField] - b[sortField]) * sortModifier;
+    });
+
+    return coursesSorted;
+    // only re-compute if these change (useMemo)
+  }, [coursesData, filterText, sortField, sortAsc]);
 
   return (
     <div className="school-catalog">
@@ -37,16 +59,16 @@ export default function SchoolCatalog() {
       <table>
         <thead>
           <tr>
-            <th>Trimester</th>
-            <th>Course Number</th>
-            <th>Courses Name</th>
-            <th>Semester Credits</th>
-            <th>Total Clock Hours</th>
+            <th onClick={() => handleSort('trimester')}>Trimester</th>
+            <th onClick={() => handleSort('courseNumber')}>Course Number</th>
+            <th onClick={() => handleSort('courseName')}>Courses Name</th>
+            <th onClick={() => handleSort('semesterCredits')}>Semester Credits</th>
+            <th onClick={() => handleSort('totalClockHours')}>Total Clock Hours</th>
             <th>Enroll</th>
           </tr>
         </thead>
         <tbody>
-          {coursesFiltered.map((course) => (
+          {coursesProcessed.map((course) => (
             <tr key={course.courseNumber}>
               <td>{course.trimester}</td>
               <td>{course.courseNumber}</td>
